35a8851679ec3e805f5afa63279046e1396a912c
tabuna <bliz48rus@gmail.com>
2017-05-27 17:31:11 +0300

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>MMA? No MMA!</title>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <link rel="stylesheet" href="/css/app.css" type="text/css"/>
+
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#58666e">
+    <meta name="theme-color" content="#f1f3f5">
+
+</head>
+<body>
+
+<header id="header" class="navbar bg-white padder-v">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="btn btn-link visible-xs pull-right m-r" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a href="#" class="navbar-brand m-r-lg"><span class="h2"><span class="text-info">Малый</span> тендер</span></a>
+        </div>
+        <div class="collapse navbar-collapse">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <div class="m-t-sm">
+                        <a href="#" class="btn btn-sm btn-info btn-rounded">Вход</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+
+
+
+<div id="content">
+
+    <div class="bg-white-only">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center">
+                    <div class="padder-v">
+                        <h2 class="font-thin text-info l-h-1x m-t-md padder-v">
+                            База проверенных поставщиков и услуг
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row v-center">
+                <div class="col-md-7 no-padder text-center">
+                    <img src="/img/image-top.png" class="img-responsive center">
+                </div>
+                <div class="col-md-5 no-padder">
+                    <p class="text-lg l-h-2x">
+                        <span class="text-info">ИМЯ</span> сайта разработан для частных лиц, фирм
+                        и предприятий, находящихся в поиске способов реализаций своих товаров и услуг через надёждных посредников. Используйте систему бесплатно до 1.09.2017 в рамках полнофункционального пробного периода
+                    </p>
+                    <p class="text-center">
+                        <a href="#" class="btn btn-lg btn-default btn-rounded">Пробный период</a>
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center">
+                    <div class="padder-v">
+                        <h1 class="font-thin l-h-1x m-t-md padder-v">
+                            Наши преимущества станут вашими
+                        </h1>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row m-b-xl m-t-xl v-center">
+                <div class="col-md-6 no-padder">
+                    <p class="h1 font-thin">
+                        <span class="text-info">01</span>
+                        <br>
+                        Интуитивная система
+                        поиска
+                    </p>
+
+
+                    <ul class="list-group no-bg no-borders pull-in m-t-lg">
+                        <li class="list-group-item">«Умные» теги</li>
+                        <li class="list-group-item">Простая сортировка стоимости товаров и услуг</li>
+                        <li class="list-group-item">Обнаружение ближайших контрагентов, соответсвующих вашим запросам</li>
+                    </ul>
+
+
+                </div>
+                <div class="col-md-6 no-padder text-center">
+                    <img src="/img/image-01.png" class="img-responsive center">
+                </div>
+            </div>
+
+
+
+            <div class="row m-b-xl m-t-xl v-center">
+                <div class="col-md-6 no-padder text-center">
+                    <img src="/img/image-02.png" class="img-responsive center">
+                </div>
+                <div class="col-md-6 no-padder">
+                    <p class="h1 font-thin">
+                        <span class="text-info">02</span>
+                        <br>
+                        Интуитивная система
+                        поиска
+                    </p>
+
+
+                    <ul class="list-group no-bg no-borders pull-in m-t-lg">
+                        <li class="list-group-item">«Умные» теги</li>
+                        <li class="list-group-item">Простая сортировка стоимости товаров и услуг</li>
+                        <li class="list-group-item">Обнаружение ближайших контрагентов, соответсвующих вашим запросам</li>
+                    </ul>
+
+
+                </div>
+            </div>
+
+
+
+            <div class="row m-b-xl m-t-xl v-center">
+                <div class="col-md-6 no-padder">
+                    <p class="h1 font-thin">
+                        <span class="text-info">03</span>
+                        <br>
+                        Интуитивная система
+                        поиска
+                    </p>
+
+
+                    <ul class="list-group no-bg no-borders pull-in m-t-lg">
+                        <li class="list-group-item">«Умные» теги</li>
+                        <li class="list-group-item">Простая сортировка стоимости товаров и услуг</li>
+                        <li class="list-group-item">Обнаружение ближайших контрагентов, соответсвующих вашим запросам</li>
+                    </ul>
+
+
+                </div>
+                <div class="col-md-6 no-padder text-center">
+                    <img src="/img/image-03.png" class="img-responsive center">
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+</div>
+
+
+
+<!-- footer -->
+<footer id="footer">
+    <div class="bg-light  b-b box-shadow-lg" style="
+            box-shadow: inset 0 0 21px rgba(0, 0, 1, 0.1);
+            ">
+        <div class="container">
+
+            <div class="row m-t-xl m-b-xl">
+                <div class="col-sm-6 text-center col-sm-offset-3">
+                    <h4 class="m-b h2">Всё ещё не уверены?</h4>
+                    <p>Обратитесь в нашу службу поддержки и мы с радостью поможем вам,
+                        или воспользуйтесь пробным периодом!</p>
+                </div>
+            </div>
+
+
+            <div class="row m-t-xl m-b-xl">
+                <div class="col-sm-6 text-right">
+                    <a href="#" class="btn btn-lg btn-default btn-rounded">Задать вопрос</a>
+                </div>
+                <div class="col-sm-6 text-left">
+                    <a href="#" class="btn btn-lg btn-default btn-rounded">Пробный период</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white b-t">
+        <div class="container">
+            <div class="row m-t-xxl m-b-xxl">
+                <div class="col-sm-3">
+                    <p>[<span class="text-info">Малый </span> тендер] © 2017</p>
+                </div>
+                <div class="col-sm-3 col-sm-offset-1">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="#">Контакты</a></li>
+                        <li><a href="#">Личный кабинет</a></li>
+                        <li><a href="#">Помощь</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3  col-sm-offset-2 text-right">
+                    8 800 900 90 90<br>
+                    mail@mail.ru
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- / footer -->
+
+
+<script src="/js/app.js"></script>
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const {mix} = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+mix.less('resources/assets/less/app.less', 'public/css/app.css');
+
+
+
+mix.copy('./node_modules/bootstrap/dist/fonts/', 'public/fonts');
+mix.copy('./node_modules/simple-line-icons/fonts/', 'public/fonts');
+mix.copy('./node_modules/font-awesome/fonts/', 'public/fonts');
+
+
+mix.js(['resources/assets/js/app.js',], '.public/js/app.js');

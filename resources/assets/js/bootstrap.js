36862badb362f6325c011376977691f294944973
tabuna <bliz48rus@gmail.com>
2017-12-11 15:10:15 +0300

    http to https

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -24,6 +24,7 @@ try {
 
 window.axios = require('axios');
 
+window.axios.defaults.baseURL = 'https://bsale.orchid.software';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
